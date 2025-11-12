55b19ad985cbc99bb463ea9ddc419cb819398e2a
sekani66 <sibabalodeyi@gmail.com>
2025-11-12 22:24:48 +0200

    Commit specific line changes

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -12,7 +12,7 @@ import protectRoute from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post("/", protectRoute, async( eq, res) => {
+router.post("/userProfile", protectRoute, async( eq, res) => {
     try {
         const { owner , identityNumber, contact, email, name, lastName, title } = req.body;
         if(!owner || !identityNumber || !contact || !email || !name || !lastName || !title)
